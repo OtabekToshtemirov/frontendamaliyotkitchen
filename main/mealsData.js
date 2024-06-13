401b60b5a5b0f8e2d2cecbd54f4b7a0d8756f149
Otabek <shoraqorgon@gmail.com>
2024-06-13 10:40:45 +0500

    process

---
+++ main/mealsData.js
@@ -0,0 +1,100 @@
+export const meals = [
+    {
+        id: 1,
+        type: 'Breakfast',
+        img: '../images/meals/as.png',
+        name: 'Chicken fricassee with mushrooms and bulgur',
+        fat: '7 g',
+        carbohydrates: '24 g',
+        energy: '285 kcal',
+        weight: '345 g'
+    },
+    {
+        id: 2,
+        type: 'Breakfast 2',
+        img: '../images/meals/gh.png',
+        name: 'Chicken fricassee with mushrooms and bulgur',
+        fat: '7 g',
+        carbohydrates: '24 g',
+        energy: '285 kcal',
+        weight: '345 g'
+    },
+    {
+        id: 3,
+        type: 'Lunch',
+        img: '../images/meals/jk.png',
+        name: 'Chicken fricassee with mushrooms and bulgur',
+        fat: '7 g',
+        carbohydrates: '24 g',
+        energy: '285 kcal',
+        weight: '345 g'
+    },
+    {
+        id: 4,
+        type: 'Snack',
+        img: '../images/meals/sd.png',
+        name: 'Chicken fricassee with mushrooms and bulgur',
+        fat: '7 g',
+        carbohydrates: '24 g',
+        energy: '285 kcal',
+        weight: '345 g'
+    },
+    {
+        id: 5,
+        type: 'Dinner',
+        img: '../images/meals/jk.png',
+        name: 'Chicken fricassee with mushrooms and bulgur',
+        fat: '7 g',
+        carbohydrates: '24 g',
+        energy: '285 kcal',
+        weight: '345 g'
+    },{
+        id: 6,
+        type: 'Dinner',
+        img: '../images/meals/jk.png',
+        name: 'Chicken fricassee with mushrooms and bulgur',
+        fat: '7 g',
+        carbohydrates: '24 g',
+        energy: '285 kcal',
+        weight: '345 g'
+    },{
+        id: 7,
+        type: 'Dinner',
+        img: '../images/meals/jk.png',
+        name: 'Chicken fricassee with mushrooms and bulgur',
+        fat: '7 g',
+        carbohydrates: '24 g',
+        energy: '285 kcal',
+        weight: '345 g'
+    },
+    {
+        id: 8,
+        type: 'Dinner',
+        img: '../images/meals/jk.png',
+        name: 'Chicken fricassee with mushrooms and bulgur',
+        fat: '7 g',
+        carbohydrates: '24 g',
+        energy: '285 kcal',
+        weight: '345 g'
+    },
+    {
+        id: 9,
+        type: 'Dinner',
+        img: '../images/meals/jk.png',
+        name: 'Chicken fricassee with mushrooms and bulgur',
+        fat: '7 g',
+        carbohydrates: '24 g',
+        energy: '285 kcal',
+        weight: '345 g'
+    },
+    {
+        id: 10,
+        type: 'Dinner',
+        img: '../images/meals/jk.png',
+        name: 'Chicken fricassee with mushrooms and bulgur',
+        fat: '7 g',
+        carbohydrates: '24 g',
+        energy: '285 kcal',
+        weight: '345 g'
+    }
+];
